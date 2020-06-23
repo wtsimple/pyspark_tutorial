7ab94e686414cb78f9a4bd2e05e2afe354e68532
Armando Rivero <armando.rivero143@gmail.com>
2020-06-23 08:13:07 -0400

    cleaned sensitive data from repo

---
+++ classification.py
@@ -0,0 +1,72 @@
+from pyspark.ml.classification import LogisticRegression, RandomForestClassifier
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.mllib.evaluation import BinaryClassificationMetrics
+
+from suprevised_learning_base import DataPreprocessor, session, ModelEvaluator
+from suprevised_learning_base import ModelTuner
+
+
+class ClassificationPreprocessor(DataPreprocessor):
+    """Makes the dataframe ready for logistic modelling"""
+    pass
+
+
+class ClassificationModelEvaluator(ModelEvaluator):
+    def __init__(self):
+        super().__init__()
+        self.metrics_class = BinaryClassificationMetrics
+        self.whole_metrics = {}
+
+
+    def append_eval_row(self, model_type, dataset, metrics):
+        if not self.whole_metrics:
+            self.whole_metrics = {"model": [], "dataset": [], "AUC_ROC": [], "AUC_PR": []}
+
+        self.whole_metrics['model'].append(model_type)
+        self.whole_metrics['dataset'].append(dataset)
+        self.whole_metrics['AUC_ROC'].append(metrics.areaUnderROC)
+        self.whole_metrics['AUC_PR'].append(metrics.areaUnderPR)
+
+
+if __name__ == "__main__":
+    # Loading
+    preprocessor = ClassificationPreprocessor()
+    evaluator = ClassificationModelEvaluator()
+
+    adult_df = session.read.csv("/home/armando/machine_learning/datasets/adult.data", header=True, inferSchema=True)
+    adult_test = session.read.csv("/home/armando/machine_learning/datasets/adult.test", header=True, inferSchema=True)
+    adult_df = preprocessor.clean_string_columns_data(adult_df)
+    adult_test = preprocessor.clean_string_columns_data(adult_test)
+
+    # Exploration
+    # preprocessor.explore_factors(train_df=adult_df, test_df=adult_test)
+
+    adult_class = preprocessor.fit(adult_df, target_col='income').transform(adult_df)
+    adult_test_class = preprocessor.transform(adult_test)
+
+    lr = LogisticRegression(maxIter=10, regParam=0.1, elasticNetParam=0.2)
+    lr_model = lr.fit(adult_class)
+
+    rfc = RandomForestClassifier(labelCol="label", featuresCol="features",
+                                 numTrees=10, cacheNodeIds=True,
+                                 subsamplingRate=0.7)
+    rfc_model = rfc.fit(adult_class)
+
+    features = evaluator.extract_feature_imp(rfc_model.featureImportances, adult_class, 'features')
+
+    # Hyperparameter tuning
+
+    tuner = ModelTuner(evaluator=BinaryClassificationEvaluator())
+    specs = {
+        'logistic_class': {"regParam": [0], "elasticNetParam": [0]},
+        # 'random_forest_class': {'numTrees': [20], "maxDepth": [5], "subsamplingRate": [0.7]},
+        'gbt_class': {},
+        # 'perceptron_class': {},
+        'svm_class': {'regParam': [0], 'standardization': [True]},
+        # 'naive_bayes_class': {},
+    }
+    models = tuner.get_crossvalidated_models(specs, adult_class)
+
+    print(evaluator.evaluation(adult_class, adult_test_class, models=models))
+
+    print('')
---
+++ regression.py
@@ -0,0 +1,57 @@
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.mllib.evaluation import RegressionMetrics
+
+from suprevised_learning_base import DataPreprocessor, session, ModelTuner, ModelEvaluator
+
+
+class RegressionModelEvaluator(ModelEvaluator):
+    def __init__(self):
+        super().__init__()
+        self.metrics_class = RegressionMetrics
+
+
+    @staticmethod
+    def append_eval_row(model_type, dataset, metrics, whole_metrics):
+        if not whole_metrics:
+            whole_metrics = {"model": [], "dataset": [], "Explained_Var": [],
+                             "Mean_Abs_Er": [], "Mean_Square_Er": [], "RMSE": [], "r2": []}
+
+        whole_metrics['model'].append(model_type)
+        whole_metrics['dataset'].append(dataset)
+        whole_metrics['Explained_Var'].append(metrics.explainedVariance)
+        whole_metrics['Mean_Abs_Er'].append(metrics.meanAbsoluteError)
+        whole_metrics['Mean_Square_Er'].append(metrics.meanSquaredError)
+        whole_metrics['RMSE'].append(metrics.rootMeanSquaredError)
+        whole_metrics['r2'].append(metrics.r2)
+
+
+if __name__ == "__main__":
+    # Loading
+    preprocessor = DataPreprocessor()
+    tuner = ModelTuner(evaluator=RegressionEvaluator())
+    evaluator = RegressionModelEvaluator()
+
+    adult_df = session.read.csv("/home/armando/machine_learning/datasets/adult.data", header=True, inferSchema=True)
+    adult_test = session.read.csv("/home/armando/machine_learning/datasets/adult.test", header=True, inferSchema=True)
+    adult_df = preprocessor.clean_string_columns_data(adult_df)
+    adult_test = preprocessor.clean_string_columns_data(adult_test)
+
+    # Exploration
+    # preprocessor.explore_factors(train_df=adult_df, test_df=adult_test)
+
+    adult_class = preprocessor.fit(adult_df, target_col='age').transform(adult_df)
+    adult_test_class = preprocessor.transform(adult_test)
+
+    specs = {
+        'linear_reg': {"regParam": [0], "elasticNetParam": [0]},
+        # 'glr_reg': {"regParam": [0]},
+        # 'decision_tree_reg': {},  # {'numTrees': [20], "maxDepth": [5], "subsamplingRate": [0.7]},
+        # 'random_forest_reg': {'numTrees': [20], "maxDepth": [5], "subsamplingRate": [0.7]},
+        # 'gbt_reg': {},
+        # 'svm_class': {'regParam': [0], 'standardization': [True]},
+    }
+    models = tuner.get_crossvalidated_models(specs, adult_class)
+
+    print(evaluator.evaluation(adult_class, adult_test_class, models=models))
+
+    print('')
