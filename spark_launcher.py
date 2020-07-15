37aacdb9450efd96dc8050569f30cb4b73e686b8
Armando Rivero <armando.rivero143@gmail.com>
2020-07-15 08:55:12 -0400

    Launcher, fixed "different versions" error

--- spark_launcher.py
+++ spark_launcher.py
@@ -1,6 +1,13 @@
+import os
+import sys
+
 from pyspark.sql import SparkSession
 
 
 class SparkLauncher(object):
     def __init__(self):
+        # Needed to prevent different worker and driver python versions error
+        for var in ['PYSPARK_PYTHON', 'PYSPARK_DRIVER_PYTHON']:
+            os.environ[var] = sys.executable
+
         self.session = SparkSession.builder.getOrCreate()
