1e1f647d13de7e51f1e8731b5029b20b861c5cba
Armando Rivero <armando.rivero143@gmail.com>
2020-07-15 08:52:06 -0400

    Launcher, reproduced error in test
    This is the error: Exception: Python in worker has different version 2.7 than that in driver 3.6, PySpark cannot run with different minor versions.Please check environment variables PYSPARK_PYTHON and PYSPARK_DRIVER_PYTHON are correctly set

--- tests/test_spark_launcher.py
+++ tests/test_spark_launcher.py
@@ -11,3 +11,16 @@ def spark():
 
 def test_spark_session_is_ok(spark):
     assert isinstance(spark.session, SparkSession)
+
+
+def test_bug_different_versions_error(spark):
+    """
+    Test intended to reproduce an error
+
+    Exception: Python in worker has different version 2.7 than that in driver 3.6, PySpark cannot run with different
+    minor versions.Please check environment variables PYSPARK_PYTHON and PYSPARK_DRIVER_PYTHON are correctly set
+    """
+    example_cols = ["column1"]
+    example_data = [["value1"], ["value2"]]
+    df = spark.session.createDataFrame(example_data, example_cols)
+    df.show()
