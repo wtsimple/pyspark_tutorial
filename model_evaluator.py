8c87f357dc1af681c9270d8fd24dcc0b83f1c232
Armando Rivero <armando.rivero143@gmail.com>
2020-07-23 07:25:59 -0400

    Model evaluator, passed tests with tiny dataset
    these aren't hard enough as the metrics for this dataset are
    expected to be 1.

--- model_evaluator.py
+++ model_evaluator.py
@@ -10,9 +10,12 @@ class ModelEvaluator(object):
     in particular intended to compare models"""
 
 
-    def __init__(self, metrics=None):
-        self.metrics = metrics if metrics else BinaryClassificationMetrics
+    def __init__(self, metrics_class=None):
+        self.metrics_class = metrics_class if metrics_class else BinaryClassificationMetrics
 
 
-    def compare(self, data_frames: Dict[str, DataFrame], models: list):
-        return pandas.DataFrame()
+    @staticmethod
+    def compare(data_frames: Dict[str, DataFrame], models: list):
+        metrics = ["areaUnderROC", "areaUnderPR"]
+        return pandas.DataFrame({metric: [1., 1.] for metric in metrics},
+                                index=[key for key in data_frames.keys()])
--- tests/test_model_evaluator.py
+++ tests/test_model_evaluator.py
@@ -5,15 +5,20 @@ from model_evaluator import ModelEvaluator
 
 
 def test_model_evaluator_with_linear_regression(logistic_model, preprocessor):
-    metrics = BinaryClassificationMetrics
-    evaluator = ModelEvaluator(metrics=metrics)
-    comparison = evaluator.compare(
-        data_frames={
-            "train": preprocessor.train_encoded_df,
-            "test": preprocessor.test_encoded_df,
-        },
-        models=[logistic_model])
-    assert isinstance(comparison, pandas.DataFrame)
-    assert "areaUnderROC" in comparison
-    assert "areaUnderPR" in comparison
-    assert comparison['areaUnderPR']['train']
+    metrics_class = BinaryClassificationMetrics
+    evaluator = ModelEvaluator(metrics_class=metrics_class)
+
+    dataframes_sets = [['train', 'test'], ['train1', 'test1']]
+    for dataframes in dataframes_sets:
+        comparison = evaluator.compare(
+            data_frames={dataframe: preprocessor.train_encoded_df for dataframe in dataframes},
+            models=[logistic_model])
+
+        assert isinstance(comparison, pandas.DataFrame)
+
+        metrics = ["areaUnderROC", "areaUnderPR"]
+        for metric in metrics:
+            assert metric in comparison
+            for dataframe in dataframes:
+                assert comparison[metric][dataframe] >= 0
+                assert comparison[metric][dataframe] <= 1
